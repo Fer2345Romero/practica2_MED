25a0c8b1e8232f47e5b7f53cd08ceca73ed47789
Fer2345Romero <140003497+Fer2345Romero@users.noreply.github.com>
2023-08-28 18:59:11 -0600

    Add files via upload

---
+++ Practica2_MED/Ejercicio_1.py
@@ -0,0 +1,4 @@
+suma = 0
+for numero in range(1, 101):
+    suma += numero
+print("La suma de los enteros del 1 al 100 es:", suma)
---
+++ Practica2_MED/Ejercicio_3.py
@@ -0,0 +1,12 @@
+numeros = [1, 4, 6, 7, 8, 10, 12, 2, 9, 28, 9, 10, 7, 3, 8]
+pares = []
+impares = []
+
+for numero in numeros:
+    pares.append(numero)
+else:
+    impares.append(numero)
+    
+print("serie:", numero)
+print("pares", pares)
+print("impares", impares)
